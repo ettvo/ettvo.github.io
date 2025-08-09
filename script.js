0fb4077aea918c119b622e4348649b3d87d092f1
Evelyn Vo <ettvo@berkeley.edu>
2025-08-08 17:07:43 -0700

    fixed issue of text not appearing in sidebar

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function open_sidebar() {
   document.getElementById("main").style.marginLeft = "25%";
-  document.getElementById("main-sidebar").style.width = "25%";
-  document.getElementById("main-sidebar").style.display = "block";
+  document.getElementById("directorySidebar").style.width = "25%";
+  document.getElementById("directorySidebar").style.display = "block";
   document.getElementById("open-sidebar-button").style.display = 'none';
 }
 
 function close_sidebar() {
   document.getElementById("main").style.marginLeft = "0%";
-  document.getElementById("main-sidebar").style.display = "none";
+  document.getElementById("directorySidebar").style.display = "none";
+  document.getElementById("directorySidebar").style.width = "0%";
   document.getElementById("open-sidebar-button").style.display = "inline-block";
 }
