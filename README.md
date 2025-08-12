b35145e8b8c6d70b6f47ac0a5ea4f542c1285ee7
Evelyn Vo <ettvo@berkeley.edu>
2025-08-11 17:37:33 -0700

    added work tabs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ettvo.github.io
+
+To-do:
+- Javascript tool on work experience to choose between 1st and 2nd jobs
+- Javascript cards to show projects? all displayed at once or select card?
--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function open_sidebar() {
-  document.getElementById("main").style.marginLeft = "25%";
-  document.getElementById("directorySidebar").style.width = "25%";
-  document.getElementById("directorySidebar").style.display = "block";
-  document.getElementById("open-sidebar-button").style.display = 'none';
-}
+// function open_sidebar() {
+//   document.getElementById("main").style.marginLeft = "25%";
+//   document.getElementById("directorySidebar").style.width = "25%";
+//   document.getElementById("directorySidebar").style.display = "block";
+//   document.getElementById("open-sidebar-button").style.display = 'none';
+// }
 
-function close_sidebar() {
-  document.getElementById("main").style.marginLeft = "0%";
-  document.getElementById("directorySidebar").style.display = "none";
-  document.getElementById("directorySidebar").style.width = "0%";
-  document.getElementById("open-sidebar-button").style.display = "inline-block";
+// function close_sidebar() {
+//   document.getElementById("main").style.marginLeft = "0%";
+//   document.getElementById("directorySidebar").style.display = "none";
+//   document.getElementById("directorySidebar").style.width = "0%";
+//   document.getElementById("open-sidebar-button").style.display = "inline-block";
+// }
+
+function openWork(evt, workplace) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(workplace).style.display = "block";
+  evt.currentTarget.className += " active";
 }
