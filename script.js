6f454454339ed78e2161431dad29172b1d4c2ab3
Evelyn Vo <ettvo@berkeley.edu>
2025-08-21 22:24:47 -0700

    work on expanding cards

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// function open_sidebar() {
-//   document.getElementById("main").style.marginLeft = "25%";
-//   document.getElementById("directorySidebar").style.width = "25%";
-//   document.getElementById("directorySidebar").style.display = "block";
-//   document.getElementById("open-sidebar-button").style.display = 'none';
-// }
-
-// function close_sidebar() {
-//   document.getElementById("main").style.marginLeft = "0%";
-//   document.getElementById("directorySidebar").style.display = "none";
-//   document.getElementById("directorySidebar").style.width = "0%";
-//   document.getElementById("open-sidebar-button").style.display = "inline-block";
-// }
-
-// document.getElementById("defaultOpenWork").click();
-
-
 function openWorkTab(evt, workplace) {
   var i, tabs, tablinks;
   tabs = document.getElementsByClassName("tabcontent");
@@ -25,7 +8,6 @@ function openWorkTab(evt, workplace) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  // document.getElementById(workplace).style.display = "block";
   document.getElementById(workplace).style.display = "contents";
   evt.currentTarget.className += " active";
 } 
@@ -34,12 +16,48 @@ function openWorkTab(evt, workplace) {
 //   document.getElementById("selected_button").click();
 // }
 
-function showProjCard(evt, card_name) {
+function showProjCardReadMore(evt, card_name) {
   tabs = document.getElementsByClassName("card_link");
   for (i = 0; i < tabs.length; i++) {
     tabs[i].style.visibility = "hidden";
   }
   document.getElementById(card_name).style.visibility = "visible";
   document.getElementById(card_name).style.animation = "typewriter 2s steps(12) infinite alternate, blink 800ms steps(12) infinite normal"
+}
 
+function showFullProjCard(evt, card_name) {
+  // const cards = document.querySelectorAll(".card");
+
+  var to_be_featured, small_cards, big_cards;
+  let big_card_id = "project_contents_card_";
+  let small_card_id = "proj_card_";
+  to_be_featured = document.getElementById(card_name);
+  small_cards = document.getElementsByClassName("project_card");
+  big_cards = document.getElementsByClassName("project_contents");
+
+  for (i = 0; i < big_cards.length; i++) {
+    big_cards[i].style.visibility = "hidden";
+  }
+
+  tablinks = document.getElementsByClassName("work_tab_buttons");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(workplace).style.display = "contents";
+  evt.currentTarget.className += " active";
+// } 
+  
+
+//   cards.forEach((card, i) => {
+//     card.addEventListener("click", () => {
+//       activateCard(card);
+//     });
+//   });
+
+//   function activateCard(card) {
+//     cards.forEach((card) => {
+//       card.classList.remove("featured");
+//     });
+//     card.classList.add("featured");
+//   }
 }
